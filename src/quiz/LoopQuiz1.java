2b287d7ddf40bc27f8dbba97c01183310bd433fd
Admin <Admin@DESKTOP-52351T8>
2024-10-30 17:59:54 +0900

    241030일 ch5 수업한 내용

--- src/quiz/LoopQuiz1.java
+++ src/quiz/LoopQuiz1.java
@@ -1,5 +1,7 @@
 package quiz;
 
+import java.util.Arrays;
+
 public class LoopQuiz1 {
 
 	public static void main(String[] args) {
@@ -10,6 +12,49 @@ public class LoopQuiz1 {
 		 * 	3. 번호를 출력하기 
 		 */
 
+		/* 배열을 사용한 코드....  */
+		// 배열의 선언... 
+		int[] lotto = new int[6];   // 정수 타입 배열로 길이 6인.... 
+		//int lotto2[] = new int[6];
+		
+		// 검증 - 중복없는 로또 번호 6개 뽑기...
+		for (int i = 0; i < 6; i++) {
+			lotto[i] = (int)(Math.random() * 45) + 1; // 1 ~ 45
+			
+			for (int j = 0; j < i ; j++) {   // 검색 알고리즘... 
+				if (lotto[j] == lotto[i]) {  
+					i++;
+					break;
+				}
+			}
+		}
+		
+		System.out.println(Arrays.toString(lotto));
+		
+		//=======================================================
+		int[] lotto2 = new int[6];
+		
+		int count = 0;
+		boolean testCheck = true;
+		while (count < 6) {
+			int lottoNum = (int)(Math.random() * 45) + 1;  	// 생성
+			for (int i = 0; i < count; i ++) {             	// 검증
+				if (lotto2[i] == lottoNum) {
+					testCheck = false;
+					break;
+				}
+			}
+			if (testCheck) {								// 대입
+				lotto2[count] = lottoNum;
+				count++;
+			}
+			testCheck = true;								// 검증 초기화
+		}
+		
+		System.out.println("while을 사용한 경우 : "+Arrays.toString(lotto2));
+		
+		
+		/*
 		int lotto1=0, lotto2=0, lotto3=0, lotto4=0, lotto5=0, lotto6=0; 
 		int count = 0; // 출력 번호 갯수
 		
@@ -42,7 +87,8 @@ public class LoopQuiz1 {
 			// 6자리 숫자가 출력된 경우 탈출	
 			if (count == 6) break;
 			
-		}	
+		}
+		*/	
 		
 	}
 
